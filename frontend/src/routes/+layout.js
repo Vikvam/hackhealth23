741e97020659822c9f5e3ba30ec8c983832bbbb7
Vikvam <vikvam@disroot.org>
2023-11-24 20:54:55 +0100

    frontend

---
+++ frontend/src/routes/+layout.js
@@ -0,0 +1,5 @@
+// Documentation: https://kit.svelte.dev/docs/page-options#prerender
+export const prerender = true;
+
+// Documentation: https://kit.svelte.dev/docs/page-options#ssr
+// TODO export const ssr = true;
