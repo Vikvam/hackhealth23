1b780fc9d4fc7fac551ac44aee901dcdc99ac2c0
Vikvam <vikvam@disroot.org>
2023-11-25 17:15:21 +0100

    dynamic DG preview skeleton

---
+++ frontend/src/routes/dg-edit/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {Label} from "flowbite-svelte";
+    import Table from "$lib/components/Table.svelte";
+
+    export let data;
+    let slug = data.slug;
+</script>
+
+<Label>
+    <span>Biopsy ID:</span>
+    {slug}
+</Label>
+
+<Table />
+
+<style>
+    span {
+        font-weight: bold;
+    }
+</style>
