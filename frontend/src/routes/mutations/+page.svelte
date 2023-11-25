6e952c57e42187a407b72e360c726631d180774e
Dias Rystin <drystin@azul.com>
2023-11-25 19:44:59 +0100

    ad

--- frontend/src/routes/mutations/+page.svelte
+++ frontend/src/routes/mutations/+page.svelte
@@ -10,7 +10,38 @@
         { id: "Region", name: "Region", field: "Region" },
         { id: "Type", name: "Type", field: "Type" },
         { id: "Reference", name: "Reference", field: "Reference" },
-        // Add more columns based on your data
+        { id: "Allele", name: "Allele", field: "Allele" },
+        { id: "Length", name: "Length", field: "Length" },
+        { id: "Count", name: "Count", field: "Count" },
+        { id: "Coverage", name: "Coverage", field: "Coverage" },
+        { id: "Frequency", name: "Frequency", field: "Frequency" },
+        {
+            id: "ForwardReverseBalance",
+            name: "Forward Reverse Balance",
+            field: "Forward Reverse Balance",
+        },
+        {
+            id: "AverageQuality",
+            name: "Average Quality",
+            field: "Average Quality",
+        },
+        { id: "GeneName", name: "Gene Name", field: "Gene Name" },
+        {
+            id: "CodingRegionChange",
+            name: "Coding Region Change",
+            field: "Coding Region Change",
+        },
+        {
+            id: "AminoAcidChange",
+            name: "Amino Acid Change",
+            field: "Amino Acid Change",
+        },
+        { id: "ExonNumber", name: "Exon Number", field: "Exon Number" },
+        {
+            id: "TypeOfMutation",
+            name: "Type of Mutation",
+            field: "Type of Mutation",
+        },
     ];
 
     var options = {
@@ -19,12 +50,12 @@
     };
 
     onMount(async () => {
-        const response = await fetch("http://localhost:8000/dg", {mode: "cors"});
+        const response = await fetch("http://localhost:8000/dg");
         const result = await response.json();
         data = result.dg;
 
         // Initialize SlickGrid with the retrieved data
-        grid = new Slick.Grid("#myGrid", data, columns, options);
+        grid = new SlickGrid("#myGrid", data, columns, options);
     });
 </script>
 
