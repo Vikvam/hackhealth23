8749ae488fb04b1effd9db9b6cdfbb96c703a1cc
Vikvam <vikvam@disroot.org>
2023-11-25 22:17:29 +0100

    dg-edit WIP

--- frontend/src/routes/dg-edit/[slug]/+page.svelte
+++ frontend/src/routes/dg-edit/[slug]/+page.svelte
@@ -22,59 +22,33 @@
     }
 
     onMount(async () => {
-        // const columns = {
-        // TODO
-        // }
-        const rows = await fetchData();
-        let columns = [
-            { id: "Chromosome", name: "Chromosome", field: "Chromosome" },
-            { id: "Region", name: "Region", field: "Region" },
-            { id: "Type", name: "Type", field: "Type" },
-            { id: "Reference", name: "Reference", field: "Reference" },
-            { id: "Allele", name: "Allele", field: "Allele" },
-            { id: "Length", name: "Length", field: "Length" },
-            { id: "Count", name: "Count", field: "Count" },
-            { id: "Coverage", name: "Coverage", field: "Coverage" },
-            { id: "Frequency", name: "Frequency", field: "Frequency" },
-            {
-                id: "ForwardReverseBalance",
-                name: "Forward Reverse Balance",
-                field: "Forward Reverse Balance",
-            },
-            {
-                id: "AverageQuality",
-                name: "Average Quality",
-                field: "Average Quality",
-            },
-            { id: "GeneName", name: "Gene Name", field: "Gene Name" },
-            {
-                id: "CodingRegionChange",
-                name: "Coding Region Change",
-                field: "Coding Region Change",
-            },
+        columns = [
+            {id: "Chromosome", name: "Chromosome", field: "Chromosome"},
+            {id: "Region", name: "Region", field: "Region"},
+            {id: "Type", name: "Type", field: "Type"},
+            {id: "Reference", name: "Reference", field: "Reference"},
+            {id: "Allele", name: "Allele", field: "Allele"},
+            {id: "Length", name: "Length", field: "Length"},
+            {id: "Count", name: "Count", field: "Count"},
+            {id: "Coverage", name: "Coverage", field: "Coverage"},
+            {id: "Frequency", name: "Frequency", field: "Frequency"},
+            {id: "ForwardReverseBalance", name: "Forward Reverse Balance", field: "Forward Reverse Balance"},
+            {id: "AverageQuality", name: "Average Quality", field: "Average Quality"},
+            {id: "GeneName", name: "Gene Name", field: "Gene Name"},
+            {id: "CodingRegionChange", name: "Coding Region Change", field: "Coding Region Change"},
             {
                 id: "AminoAcidChange",
                 name: "Amino Acid Change",
                 field: "Amino Acid Change",
             },
-            { id: "ExonNumber", name: "Exon Number", field: "Exon Number" },
+            {id: "ExonNumber", name: "Exon Number", field: "Exon Number"},
             {
                 id: "TypeOfMutation",
                 name: "Type of Mutation",
                 field: "Type of Mutation",
             },
         ];
-        for (let i = 0; i < 50; i++) {
-            rows[i] = {
-                id: i,
-                title: "Task " + i,
-                duration: "5 days",
-                percentComplete: Math.round(Math.random() * 100),
-                start: "01/01/2009",
-                finish: "01/05/2009",
-                effortDriven: i % 5 == 0,
-            };
-        }
+        rows = await fetchData();
         mounted = true;
     });
 </script>
