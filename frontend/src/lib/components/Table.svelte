4436b97d606b3064cc9e1dd0b8800df55a627f5b
Vikvam <vikvam@disroot.org>
2023-11-25 20:26:33 +0100

    Exploring SlickDataView

--- frontend/src/lib/components/Table.svelte
+++ frontend/src/lib/components/Table.svelte
@@ -1,11 +1,13 @@
 <script type="module">
+    import { onMount } from "svelte";
     import {
         SlickGrid,
+        SlickDataView,
         CheckboxFormatter
     } from "slickgrid";
 
     let grid;
-    // let dataView = new DataView({inlineFilters: true});
+    let dataView;
 
     const columns = [
         {id: "title", name: "Title", field: "title"},
@@ -15,12 +17,24 @@
         {id: "finish", name: "Finish", field: "finish"},
         {id: "effort-driven", name: "Effort Driven", field: "effortDriven", formatter: CheckboxFormatter}
     ];
+    let columnFilters = {}
+
+    function filter(item) {
+        for (var columnId in columnFilters) {
+            if (columnId !== undefined && columnFilters[columnId] !== "") {
+                var c = grid.getColumns()[grid.getColumnIndex(columnId)];
+                if (item[c.field] != columnFilters[columnId]) return false;
+            }
+        }
+        return true;
+    }
 
     console.log(CheckboxFormatter)
 
     var options = {
         enableCellNavigation: true,
-        enableColumnReorder: false
+        enableColumnReorder: false,
+        showHeaderRow: true,
     };
 
     let data = [];
@@ -35,11 +49,11 @@
         };
     }
 
-    function renderTable() {
+    onMount(async () => {
+        dataView = new SlickDataView({});
         grid = new SlickGrid("#slickgrid", data, columns, options);
-    }
+    })
 </script>
 
 <div id="slickgrid" style="width:100%;height:500px;"></div>
 
-<svelte:window on:load={renderTable}></svelte:window>
